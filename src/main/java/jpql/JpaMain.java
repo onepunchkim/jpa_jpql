8a94485b1ee23c133ded05859d7c7c94fa2de281
onepunchkim <hyukjin4711@gmail.com>
2026-01-02 18:04:19 +0900

    jpql projection

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -19,9 +21,22 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
 
             em.flush();
+            em.clear();
+
+            //영속성 컨텍스트에서 관리된다.
+            //List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+
+            //DTO클래스와 생성자를 통해서 생성
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("username = " + memberDTO.getUsername());
+            System.out.println("age = " + memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e) {
